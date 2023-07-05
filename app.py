dd22892e718d8ae1953d7fc01d12de3beda72f9e
Rishika631 <89201634+Rishika631@users.noreply.github.com>
2023-07-05 19:43:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ from bs4 import BeautifulSoup
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
 from scrapy import Spider, Request
-from scrapy.selector import Selector
 
 
 def scrape_mayo_clinic(query):
@@ -15,104 +14,57 @@ def scrape_mayo_clinic(query):
     # Process and extract relevant information from the webpage
     # Return the response to be shown to the user
 
-# Function to scrape WebMD using the Spider
+
 class WebmdSpider(Spider):
     name = "webmd"
-    allowed_urls = ['http://www.webmd.com/']
-    start_urls = ['http://www.webmd.com/drugs/2/index']
+    allowed_domains = ['webmd.com']
+    start_urls = []
+
+    def __init__(self, query, **kwargs):
+        super().__init__(**kwargs)
+        self.start_urls = [f'https://www.webmd.com/drugs/2/search?type=name&query={query}']
 
     def parse(self, response):
-        atoz = response.xpath('//ul[@class="browse-letters squares"]')[0].xpath("li/a/@href").extract()
-        for i in range(len(atoz)):
-            yield Request(response.urljoin(atoz[i]), callback=self.parse_sub, dont_filter=True)
-
-    def parse_sub(self, response):
-        sub = response.xpath('//ul[@class="browse-subletters squares"]')[0].xpath("li/a/@href").extract()
-        for i in range(len(sub)):
-            yield Request(response.urljoin(sub[i]), callback=self.parse_drug, dont_filter=True)
-
-    def parse_drug(self, response):
-        drug_list = response.xpath('//ul[@class="drug-list"]')[0].xpath("li/a")
-        for i in range(len(drug_list)):
-            yield Request(response.urljoin(drug_list[i].xpath("@href")[0].extract()), callback=self.parse_details, dont_filter=True)
+        drug_list = response.xpath('//ul[@class="search-list"]/li/a[@class="search-link"]')
+        for drug in drug_list:
+            yield Request(response.urljoin(drug.xpath("@href").extract_first()), callback=self.parse_details)
 
     def parse_details(self, response):
-        Use = ' '.join(response.xpath('//h3[contains(text(), "Uses")]/following-sibling::p//text()').extract())
-        HowtoUse = ' '.join(response.xpath('//h3[contains(text(), "How to use")]/following-sibling::p//text()').extract())
-        Sides = ' '.join(response.xpath('//h3[contains(text(), "Side Effects")]/following-sibling::div/p//text()').extract()).replace('\r\n', '')
-        Precautions = ' '.join(response.xpath('//h3[contains(text(), "Precautions")]/following-sibling::div/p//text()').extract())
-        Interactions = ' '.join(response.xpath('//h3[contains(text(), "Interactions")]/following-sibling::div/p//text()').extract())
+        name = response.xpath('//h1/text()').extract_first()
+        uses = ' '.join(response.xpath('//h3[contains(text(), "Uses")]/following-sibling::p//text()').extract())
+        how_to_use = ' '.join(response.xpath('//h3[contains(text(), "How to Use")]/following-sibling::p//text()').extract())
+        side_effects = ' '.join(response.xpath('//h3[contains(text(), "Side Effects")]/following-sibling::div/p//text()').extract())
+        precautions = ' '.join(response.xpath('//h3[contains(text(), "Precautions")]/following-sibling::div/p//text()').extract())
+        interactions = ' '.join(response.xpath('//h3[contains(text(), "Interactions")]/following-sibling::div/p//text()').extract())
 
-        # Clean up the extracted data
-        Use = Use.strip() if Use else ''
-        HowtoUse = HowtoUse.strip() if HowtoUse else ''
-        Sides = Sides.strip() if Sides else ''
-        Precautions = Precautions.strip() if Precautions else ''
-        Interactions = Interactions.strip() if Interactions else ''
+        result = f"Name: {name}\n\n" \
+                 f"Uses: {uses}\n\n" \
+                 f"How to Use: {how_to_use}\n\n" \
+                 f"Side Effects: {side_effects}\n\n" \
+                 f"Precautions: {precautions}\n\n" \
+                 f"Interactions: {interactions}\n\n"
 
-        # Create a dictionary with the extracted data
-        drug_data = {
-            'Use': Use,
-            'HowtoUse': HowtoUse,
-            'Sides': Sides,
-            'Precautions': Precautions,
-            'Interactions': Interactions
-        }
-
-        yield drug_data
-
+        st.write(result)
 
 
 def get_answer(query, sources):
-    answers = []
-
     if "Mayo Clinic" in sources:
-        answers.extend(scrape_mayo_clinic(query))
+        scrape_mayo_clinic(query)
 
     if "WebMD" in sources:
-        # Create a CrawlerProcess and get the settings
         process = CrawlerProcess(get_project_settings())
-
-        # Create a list to store the scraped data
-        scraped_data = []
-
-        # Create a callback function to handle the scraped data
-        def handle_data(item, response, spider):
-            scraped_data.append(item)
-
-        # Start the spider and pass the query as an argument
         process.crawl(WebmdSpider, query=query)
-        process.crawl(WebmdSpider, query=query, handle_data=handle_data)
         process.start()
 
-        # Process the scraped data and add it to the answers list
-        for item in scraped_data:
-            answer = f"Use: {item['Use']}\n\n" \
-                     f"How to Use: {item['HowtoUse']}\n\n" \
-                     f"Side Effects: {item['Sides']}\n\n" \
-                     f"Precautions: {item['Precautions']}\n\n" \
-                     f"Interactions: {item['Interactions']}\n\n"
-            answers.append(answer)
-
-    # Implement logic to select the best answer based on relevance or any other criteria
-
-    return answers
 
 def main():
     st.title("Medical Chatbot")
     query = st.text_input("Ask a medical question")
 
     if st.button("Search"):
-        # Perform question answering based on the selected sources
         selected_sources = st.multiselect("Select sources", ["Mayo Clinic", "WebMD"])
-        answers = get_answer(query, selected_sources)
+        get_answer(query, selected_sources)
 
-        # Display the answers
-        if answers:
-            for answer in answers:
-                st.write(answer)
-        else:
-            st.write("No relevant answers found.")
 
 if __name__ == "__main__":
     main()
