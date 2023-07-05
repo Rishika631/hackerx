87ac05a6da79aa3796feb0d69e678301ba0ddc04
Rishika631 <89201634+Rishika631@users.noreply.github.com>
2023-07-05 19:17:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import requests
 from bs4 import BeautifulSoup
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
-import WebmdSpider
 
 def scrape_mayo_clinic(query):
     url = f"https://www.mayoclinic.org/search/search-results?q={query}"
