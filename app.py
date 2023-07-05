4e7c76248e9a6242909375ee21340f84b3be3778
Rishika631 <89201634+Rishika631@users.noreply.github.com>
2023-07-05 20:33:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,9 @@
 import requests
 import streamlit as st
 
-API_URL = 'https://healthapi.dev/api'
-
-# Function to fetch information about a specific medicine
-def fetch_medicine_info(medicine_name):
-    url = f'{API_URL}/medications?name={medicine_name}'
-    response = requests.get(url)
-    data = response.json()
-    return data
-
-# Function to fetch information about a specific disease
-def fetch_disease_info(disease_name):
-    url = f'{API_URL}/diseases?name={disease_name}'
+# Function to fetch drug information from OpenFDA API
+def fetch_drug_information(drug_name):
+    url = f'https://api.fda.gov/drug/label.json?search=brand_name:{drug_name}'
     response = requests.get(url)
     data = response.json()
     return data
@@ -20,41 +11,25 @@ def fetch_disease_info(disease_name):
 # Streamlit app
 def main():
     st.title("Health Bot")
-    option = st.sidebar.selectbox("Select an option:", ("Medicine", "Disease"))
+    st.write("Enter a drug name to fetch information:")
 
-    if option == "Medicine":
-        st.write("Enter a medicine name to fetch information:")
-        medicine_name = st.text_input("Medicine Name")
+    # User input for drug name
+    drug_name = st.text_input("Drug Name")
 
-        if st.button("Fetch Information"):
-            data = fetch_medicine_info(medicine_name)
+    if st.button("Fetch Information"):
+        # Fetch drug information
+        data = fetch_drug_information(drug_name)
 
-            if 'error' in data:
-                st.write(f"Error: {data['error']}")
-            else:
-                st.write("### Medicine Information")
-                st.write(f"Name: {data['name']}")
-                st.write(f"Description: {data['description']}")
-                st.write(f"Usage: {data['usage']}")
-                st.write(f"Precautions: {data['precautions']}")
-                st.write(f"Side Effects: {data['side_effects']}")
-                st.write(f"Contraindications: {data['contraindications']}")
-
-    elif option == "Disease":
-        st.write("Enter a disease name to fetch information:")
-        disease_name = st.text_input("Disease Name")
-
-        if st.button("Fetch Information"):
-            data = fetch_disease_info(disease_name)
-
-            if 'error' in data:
-                st.write(f"Error: {data['error']}")
-            else:
-                st.write("### Disease Information")
-                st.write(f"Name: {data['name']}")
-                st.write(f"Description: {data['description']}")
-                st.write(f"Symptoms: {', '.join(data['symptoms'])}")
-                st.write(f"Treatments: {', '.join(data['treatments'])}")
+        # Display results
+        if 'results' in data and len(data['results']) > 0:
+            drug_info = data['results'][0]
+            st.write("### Drug Information")
+            st.write(f"Brand Name: {drug_info.get('openfda', {}).get('brand_name', ['N/A'])[0]}")
+            st.write(f"Generic Name: {drug_info.get('openfda', {}).get('generic_name', ['N/A'])[0]}")
+            st.write(f"Manufacturer: {drug_info.get('openfda', {}).get('manufacturer_name', ['N/A'])[0]}")
+            st.write(f"Indications and Usage: {drug_info.get('indications_and_usage', 'N/A')}")
+        else:
+            st.write("No information found for the given drug.")
 
 if __name__ == '__main__':
     main()
