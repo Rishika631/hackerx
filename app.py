355532d29bc9eb2874edcbbb0624c91f6305c72a
Rishika631 <89201634+Rishika631@users.noreply.github.com>
2023-07-05 20:50:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,35 +1,36 @@
 import requests
 import streamlit as st
 
-# Function to fetch health information from FAERS API
-def fetch_health_information(question):
-    url = f"https://api.fda.gov/drug/event.json?limit=1&search={question}"
-    response = requests.get(url)
+API_ENDPOINT = 'https://mcc.api.mayo.edu/semantic/v2/ask'
+
+def fetch_mayo_clinic_information(query):
+    headers = {
+        'Content-Type': 'application/json'
+    }
+
+    data = {
+        'question': query,
+        'target': 'web'
+    }
+
+    response = requests.post(API_ENDPOINT, headers=headers, json=data)
     data = response.json()
     return data
 
-# Streamlit app
 def main():
     st.title("Health Bot")
-    st.write("Enter a question to fetch health-related information:")
+    st.write("Enter a health-related question:")
 
-    # User input for question
     question = st.text_input("Question")
 
     if st.button("Fetch Information"):
-        # Fetch health information
-        data = fetch_health_information(question)
+        data = fetch_mayo_clinic_information(question)
 
-        # Display results
-        if 'results' in data and len(data['results']) > 0:
-            event_info = data['results'][0]
+        if 'answers' in data and len(data['answers']) > 0:
+            answer = data['answers'][0]
             st.write("### Health Information")
             st.write(f"Question: {question}")
-            st.write(f"Event Date: {event_info.get('receivedate', 'N/A')}")
-            st.write(f"Event Type: {event_info.get('primarysource', {}).get('report_type', 'N/A')}")
-            st.write(f"Patient Age: {event_info.get('patient', {}).get('patientage', 'N/A')}")
-            st.write(f"Patient Sex: {event_info.get('patient', {}).get('patientsex', 'N/A')}")
-            st.write(f"Description: {event_info.get('patient', {}).get('reaction', ['N/A'])[0].get('reactionmeddrapt', 'N/A')}")
+            st.write(f"Answer: {answer['content']}")
         else:
             st.write("No information found for the given question.")
 
