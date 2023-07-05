8aecb6bdcff68e536af7e709acddac8dd382f216
Rishika631 <89201634+Rishika631@users.noreply.github.com>
2023-07-05 20:05:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,33 +10,35 @@ def scrape_mayo_clinic(query):
     url = f"https://www.mayoclinic.org/search/search-results?q={query}"
     response = requests.get(url)
     soup = BeautifulSoup(response.content, "html.parser")
-    
+
     # Process and extract relevant information from the webpage
     search_results = soup.find_all("li", class_="search")
-    
+
     if search_results:
         # Extract the first search result
         first_result = search_results[0]
-        
+
         # Get the link to the detailed page
         link = first_result.find("a")["href"]
-        
+
         # Retrieve the detailed page content
         detailed_response = requests.get(link)
         detailed_soup = BeautifulSoup(detailed_response.content, "html.parser")
-        
+
         # Extract relevant information from the detailed page
         drug_name = detailed_soup.find("h1", class_="drug-name-title").text.strip()
         uses = detailed_soup.find("section", class_="drug-section drug-section-uses").text.strip()
         side_effects = detailed_soup.find("section", class_="drug-section drug-section-side-effects").text.strip()
-        
-        # Return the extracted information
+
+        # Prepare the result
         result = f"Name: {drug_name}\n\n" \
                  f"Uses: {uses}\n\n" \
                  f"Side Effects: {side_effects}\n\n"
-        return result
+
+        # Write the result to Streamlit
+        st.write(result)
     else:
-        return "No results found"
+        st.write("No results found")
 
 
 
