d5ca8e4a907d00241080b03c73f80aca0fd3e104
Rishika631 <89201634+Rishika631@users.noreply.github.com>
2023-07-14 08:03:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -144,6 +144,9 @@ def main():
         uploaded_file = st.file_uploader("Upload Image", type=['jpg', 'jpeg', 'png'])
         
         if uploaded_file is not None:
+            image = Image.open(uploaded_file)
+            st.image(image, use_column_width=True)
+            
             image_bytes = uploaded_file.read()
             output = query(image_bytes)
             # Display the generated captions
