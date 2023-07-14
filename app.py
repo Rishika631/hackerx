2d527d45e907af1b309f5d57a75944650a975380
Rishika631 <89201634+Rishika631@users.noreply.github.com>
2023-07-14 06:50:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from io import BytesIO
 import requests
 import json
 import base64
+import boto3
 from requests_toolbelt.multipart.encoder import MultipartEncoder
 
 # Set your OpenAI API credentials
@@ -44,33 +45,51 @@ def apply_frame(image, padding):
 
 # AI-powered Image Analysis and Tagging
 
-def analyze_image(image):
-    endpoint = "https://api.openai.com/v1/engines/davinci-codex/completions"
-    headers = {
-        "Authorization": f"Bearer {API_KEY}",
-        "Content-Type": "application/json",
-    }
-    data = {
-        "prompt": "Generate tags for the given image:",
-        "max_tokens": 20,
-        "temperature": 0.5,
-        "top_p": 1.0,
-        "n": 5,
-        "stop": ["\n"],
-        "inputs": {
-            "image": base64.b64encode(image).decode("utf-8"),
-        },
-    }
+client=boto3.client('rekognition')
 
-    response = requests.post(endpoint, headers=headers, json=data)
-    try:
-        response.raise_for_status()
-        tags = response.json()["choices"][0]["text"].strip().split(",")
-        return tags
-    except requests.exceptions.HTTPError as err:
-        print("HTTP Error:", err)
-        print("Response content:", response.content)
-        return []
+def image_caption_generator(image_path):
+    
+    # create a tmp folder in order to save the resized input image
+    if not os.path.exists('tmp'):
+        os.makedirs('tmp')
+    
+    # Open the original image
+    img = Image.open(image_path)  
+
+    # Set the desired size for the resized image
+    new_size = (100, 100)  
+
+    # Resize the image
+    resized_img = img.resize(new_size)
+
+    # Save the resized image
+    resized_img.save('tmp/tmp.jpg') 
+    
+    with open('tmp.jpg', 'rb') as image:
+        response = client.detect_labels(Image={'Bytes': image.read()})
+    
+    image_labels = []
+    for label in response['Labels']:
+        if label['Confidence']>70:
+            image_labels.append(label['Name'].lower())
+
+    # Generate a prompt by concatenating the image labels
+    prompt = 'Generate an image caption for the following image labels: ' + ', '.join(image_labels)
+
+    # Use the OpenAI API to generate image captions
+    response = openai.Completion.create(
+      model='text-davinci-003',
+      prompt=prompt,
+      temperature=0.5,
+      max_tokens=50
+    )
+
+    # Extract the generated image captions from the API response
+    generated_captions = response['choices'][0]['text']
+    
+    output = 'Generated Image Captions:\n'+generated_captions
+
+    return output
 
 
 # Image Resize with AI Analysis
