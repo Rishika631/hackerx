13631e0c5185fc3129cfa5f54a54614cd2a6c475
Rishika631 <89201634+Rishika631@users.noreply.github.com>
2023-07-14 06:19:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,62 +1,105 @@
-import requests
 import streamlit as st
+import requests
+from PIL import Image, ImageFilter, ImageEnhance, ImageOps
 
-MAYO_API_ENDPOINT = 'https://mcc.api.mayo.edu/semantic/v2/ask'
-WEBMD_API_ENDPOINT = 'https://www.webmd.com/'
+# Set your OpenAI API credentials
+API_KEY = "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"
 
-def fetch_mayo_clinic_information(query):
-    headers = {
-        'Content-Type': 'application/json'
-    }
-
-    data = {
-        'question': query,
-        'target': 'web'
-    }
-
-    response = requests.post(MAYO_API_ENDPOINT, headers=headers, json=data)
-    data = response.json()
-    return data
-
-def fetch_webmd_information(query):
-    headers = {
-        'Content-Type': 'application/json'
-    }
-
-    data = {
-        'question': query,
-        'target': 'web'
-    }
-
-    response = requests.post(WEBMD_API_ENDPOINT, headers=headers, json=data)
-    data = response.json()
-    return data
-
+# Front-end
 def main():
-    st.title("Health Bot")
-    st.write("Enter a health-related question:")
+    st.title("Digital Asset Management App")
 
-    question = st.text_input("Question")
+    # Add a sidebar with function selection
+    function = st.sidebar.selectbox("Select Function", ["Image Transformation", "AI Analysis", "Image Optimization"])
 
-    if st.button("Fetch Information"):
-        mayo_data = fetch_mayo_clinic_information(question)
-        webmd_data = fetch_webmd_information(question)
+    if function == "Image Transformation":
+        # Image Transformation
+        uploaded_image = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
+        if uploaded_image is not None:
+            transformed_image = perform_image_transformation(uploaded_image)
+            st.image(transformed_image, use_column_width=True)
 
-        st.write("### Mayo Clinic Information")
-        if 'answers' in mayo_data and len(mayo_data['answers']) > 0:
-            answer = mayo_data['answers'][0]
-            st.write(f"Question: {question}")
-            st.write(f"Answer: {answer['content']}")
-        else:
-            st.write("No information found from Mayo Clinic API.")
+    elif function == "AI Analysis":
+        # AI Analysis and Tagging
+        uploaded_image = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
+        if uploaded_image is not None:
+            tags = perform_image_analysis(uploaded_image)
+            st.write("Tags:", tags)
 
-        st.write("### WebMD Information")
-        if 'data' in webmd_data and 'article' in webmd_data['data']:
-            article = webmd_data['data']['article']
-            st.write(f"Title: {article['title']}")
-            st.write(f"Summary: {article['summary']}")
-        else:
-            st.write("No information found from WebMD API.")
+    elif function == "Image Optimization":
+        # Image Optimization
+        uploaded_image = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
+        if uploaded_image is not None:
+            width = st.sidebar.slider("Width", 100, 2000, 800, 100)
+            height = st.sidebar.slider("Height", 100, 2000, 600, 100)
+            optimized_image = perform_image_optimization(uploaded_image, width, height)
+            st.image(optimized_image, use_column_width=True)
 
-if __name__ == '__main__':
+# Image Transformation
+def perform_image_transformation(uploaded_image):
+    # Open the uploaded image using Pillow
+    image = Image.open(uploaded_image)
+
+    # Crop the image
+    cropped_image = image.crop((100, 100, 300, 300))
+
+    # Transform the image
+    transformed_image = cropped_image.rotate(45)
+
+    # Apply a focal point effect
+    blurred_image = transformed_image.filter(ImageFilter.GaussianBlur(radius=10))
+
+    # Apply brightness effect
+    enhancer = ImageEnhance.Brightness(blurred_image)
+    brightness_image = enhancer.enhance(1.5)
+
+    # Apply overlay
+    overlay = Image.open("overlay.png").convert("RGBA")
+    overlayed_image = Image.alpha_composite(brightness_image.convert("RGBA"), overlay)
+
+    # Apply frame (padding)
+    padded_image = ImageOps.expand(overlayed_image, border=20, fill="white")
+
+    return padded_image
+
+# AI Analysis and Tagging
+def perform_image_analysis(uploaded_image):
+    # Upload the image to OpenAI API for analysis
+    response = upload_image_to_openai(uploaded_image)
+
+    # Get the generated image tags from the OpenAI API response
+    tags = response["output"]["tags"]
+
+    return tags
+
+# Image Optimization
+def perform_image_optimization(uploaded_image, width, height):
+    # Open the uploaded image using Pillow
+    image = Image.open(uploaded_image)
+
+    # Resize the image for optimization
+    resized_image = image.resize((width, height))
+
+    # Convert the image to WebP format
+    resized_image.save("optimized_image.webp", "webp", quality=80)
+
+    # Open the optimized image
+    optimized_image = Image.open("optimized_image.webp")
+
+    return optimized_image
+
+# Upload image to OpenAI API for analysis
+def upload_image_to_openai(uploaded_image):
+    headers = {
+        "Authorization": f"Bearer {API_KEY}",
+        "Content-Type": "image/jpeg"  # Adjust the content type based on the uploaded image type
+    }
+
+    files = {"file": uploaded_image.read()}
+
+    response = requests.post("https://api.openai.com/v1/vision/davinci/tags", headers=headers, files=files)
+    response.raise_for_status()
+    return response.json()
+
+if __name__ == "__main__":
     main()
