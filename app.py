7fd1681b2aed72159e191d85c7d6fdd120aa4656
Rishika631 <89201634+Rishika631@users.noreply.github.com>
2023-07-14 06:27:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,96 +1,135 @@
 import streamlit as st
 import requests
-from PIL import Image, ImageFilter, ImageEnhance, ImageOps
+from PIL import Image, ImageOps, ImageEnhance, ImageFilter
 
 # Set your OpenAI API credentials
 API_KEY = "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"
 
-# Front-end
+# Image Transformation: Crop
+def crop_image(image, left, top, right, bottom):
+    return image.crop((left, top, right, bottom))
+
+# Image Transformation: Transform
+def transform_image(image, angle, scale):
+    transformed_image = image.rotate(angle)
+    transformed_image = transformed_image.resize((int(image.width * scale), int(image.height * scale)))
+    return transformed_image
+
+# Image Transformation: Focal Point
+def apply_focal_point(image, x, y, size):
+    blurred_image = image.filter(ImageFilter.GaussianBlur(radius=10))
+    cropped_image = crop_image(blurred_image, x - size, y - size, x + size, y + size)
+    return cropped_image
+
+# Image Transformation: Effects (Brightness)
+def apply_brightness(image, factor):
+    enhancer = ImageEnhance.Brightness(image)
+    enhanced_image = enhancer.enhance(factor)
+    return enhanced_image
+
+# Image Transformation: Overlay
+def apply_overlay(image, overlay_path):
+    overlay = Image.open(overlay_path).convert("RGBA")
+    overlayed_image = Image.alpha_composite(image.convert("RGBA"), overlay)
+    return overlayed_image
+
+# Image Transformation: Frames
+def apply_frame(image, padding):
+    framed_image = ImageOps.expand(image, border=padding, fill="white")
+    return framed_image
+
+# AI-powered Image Analysis and Tagging
+def analyze_image(image):
+    headers = {
+        "Authorization": f"Bearer {API_KEY}",
+        "Content-Type": "image/jpeg"
+    }
+    files = {"file": image}
+    response = requests.post("https://api.openai.com/v1/vision/davinci/tags", headers=headers, files=files)
+    response.raise_for_status()
+    return response.json()["output"]["tags"]
+
+# Image Resize with AI Analysis
+def resize_image_with_analysis(image, width, height):
+    resized_image = image.resize((width, height))
+    tags = analyze_image(resized_image)
+    return resized_image, tags
+
+# Streamlit App
 def main():
     st.title("Digital Asset Management App")
 
     # Add a sidebar with function selection
-    function = st.sidebar.selectbox("Select Function", ["Image Transformation", "AI Analysis", "Image Optimization"])
+    function = st.sidebar.selectbox("Select Function", ["Image Transformation", "AI Analysis", "Image Resize"])
 
     if function == "Image Transformation":
         # Image Transformation
         uploaded_image = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
-        overlay_image = st.file_uploader("Upload overlay image", type=["jpg", "jpeg", "png"])
-        if uploaded_image is not None and overlay_image is not None:
-            transformed_image = perform_image_transformation(uploaded_image, overlay_image)
+        if uploaded_image is not None:
+            image = Image.open(uploaded_image)
+
+            # Crop
+            st.subheader("Crop")
+            left = st.slider("Left", 0, image.width, 0)
+            top = st.slider("Top", 0, image.height, 0)
+            right = st.slider("Right", 0, image.width, image.width)
+            bottom = st.slider("Bottom", 0, image.height, image.height)
+            cropped_image = crop_image(image, left, top, right, bottom)
+            st.image(cropped_image, use_column_width=True)
+
+            # Transform
+            st.subheader("Transform")
+            angle = st.slider("Angle", -180, 180, 0)
+            scale = st.slider("Scale", 0.1, 5.0, 1.0)
+            transformed_image = transform_image(image, angle, scale)
             st.image(transformed_image, use_column_width=True)
+
+            # Focal Point
+            st.subheader("Focal Point")
+            x = st.slider("X", 0, image.width, int(image.width / 2))
+            y = st.slider("Y", 0, image.height, int(image.height / 2))
+            size = st.slider("Size", 10, 200, 50)
+            focal_point_image = apply_focal_point(image, x, y, size)
+            st.image(focal_point_image, use_column_width=True)
+
+            # Effects (Brightness)
+            st.subheader("Effects (Brightness)")
+            factor = st.slider("Factor", 0.1, 2.0, 1.0)
+            brightness_image = apply_brightness(image, factor)
+            st.image(brightness_image, use_column_width=True)
+
+            # Overlay
+            st.subheader("Overlay")
+            overlay_image = st.file_uploader("Upload an overlay image", type=["png"])
+            if overlay_image is not None:
+                overlay = Image.open(overlay_image)
+                overlayed_image = apply_overlay(image, overlay)
+                st.image(overlayed_image, use_column_width=True)
+
+            # Frames
+            st.subheader("Frames")
+            padding = st.slider("Padding", 0, 50, 10)
+            framed_image = apply_frame(image, padding)
+            st.image(framed_image, use_column_width=True)
 
     elif function == "AI Analysis":
         # AI Analysis and Tagging
         uploaded_image = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
         if uploaded_image is not None:
-            tags = perform_image_analysis(uploaded_image)
+            image = Image.open(uploaded_image)
+            tags = analyze_image(image)
             st.write("Tags:", tags)
 
-    elif function == "Image Optimization":
-        # Image Optimization
+    elif function == "Image Resize":
+        # Image Resize with AI Analysis
         uploaded_image = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
         if uploaded_image is not None:
-            width = st.sidebar.slider("Width", 100, 2000, 800, 100)
-            height = st.sidebar.slider("Height", 100, 2000, 600, 100)
-            optimized_image = perform_image_optimization(uploaded_image, width, height)
-            st.image(optimized_image, use_column_width=True)
-
-# Image Transformation
-def perform_image_transformation(uploaded_image, overlay_image):
-    # Open the uploaded image using Pillow
-    image = Image.open(uploaded_image)
-
-    # Add your image transformation logic here
-    # This is a placeholder code that returns the uploaded image as is
-    transformed_image = image
-
-    # Open the uploaded overlay image using Pillow
-    overlay = Image.open(overlay_image).convert("RGBA")
-
-    # Apply overlay
-    overlayed_image = Image.alpha_composite(transformed_image.convert("RGBA"), overlay)
-
-    return overlayed_image
-
-# AI Analysis and Tagging
-def perform_image_analysis(uploaded_image):
-    # Upload the image to OpenAI API for analysis
-    response = upload_image_to_openai(uploaded_image)
-
-    # Get the generated image tags from the OpenAI API response
-    tags = response["output"]["tags"]
-
-    return tags
-
-# Image Optimization
-def perform_image_optimization(uploaded_image, width, height):
-    # Open the uploaded image using Pillow
-    image = Image.open(uploaded_image)
-
-    # Resize the image for optimization
-    resized_image = image.resize((width, height))
-
-    # Convert the image to WebP format
-    resized_image.save("optimized_image.webp", "webp", quality=80)
-
-    # Open the optimized image
-    optimized_image = Image.open("optimized_image.webp")
-
-    return optimized_image
-
-# Upload image to OpenAI API for analysis
-def upload_image_to_openai(uploaded_image):
-    headers = {
-        "Authorization": f"Bearer {API_KEY}",
-        "Content-Type": "image/jpeg"  # Adjust the content type based on the uploaded image type
-    }
-
-    files = {"file": uploaded_image.read()}
-
-    response = requests.post("https://api.openai.com/v1/vision/davinci/tags", headers=headers, files=files)
-    response.raise_for_status()
-    return response.json()
+            image = Image.open(uploaded_image)
+            width = st.slider("Width", 100, 2000, 800, 100)
+            height = st.slider("Height", 100, 2000, 600, 100)
+            resized_image, tags = resize_image_with_analysis(image, width, height)
+            st.image(resized_image, use_column_width=True)
+            st.write("Tags:", tags)
 
 if __name__ == "__main__":
     main()
