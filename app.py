9f1b2b793ca7f2bc4bc6bfbb20e7f4a03f746f41
Rishika631 <89201634+Rishika631@users.noreply.github.com>
2023-07-14 07:43:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -64,13 +64,18 @@ def query(file):
     response = requests.post(API_URL, headers=headers, data=file)
     return response.text
 
+# Image Resize with AI Analysis
+def resize_image_with_analysis(image, width, height):
+    resized_image = image.resize((width, height))
+    output = query(resized_image)
+    return resized_image, output
 
 # Streamlit App
 def main():
     st.title("Digital Asset Management App")
 
     # Add a sidebar with function selection
-    function = st.sidebar.selectbox("Select Function", ["Image Transformation", "AI Analysis"])
+    function = st.sidebar.selectbox("Select Function", ["Image Transformation", "AI Analysis", "Image Resize"])
 
     if function == "Image Transformation":
         # Image Transformation
@@ -78,49 +83,8 @@ def main():
         if uploaded_image is not None:
             image = Image.open(uploaded_image)
 
-            # Crop
-            st.subheader("Crop")
-            left = st.slider("Left", 0, image.width, 0)
-            top = st.slider("Top", 0, image.height, 0)
-            right = st.slider("Right", 0, image.width, image.width)
-            bottom = st.slider("Bottom", 0, image.height, image.height)
-            cropped_image = crop_image(image, left, top, right, bottom)
-            st.image(cropped_image, use_column_width=True)
-
-            # Transform
-            st.subheader("Transform")
-            angle = st.slider("Angle", -180, 180, 0)
-            scale = st.slider("Scale", 0.1, 5.0, 1.0)
-            transformed_image = transform_image(image, angle, scale)
-            st.image(transformed_image, use_column_width=True)
-
-            # Focal Point
-            st.subheader("Focal Point")
-            x = st.slider("X", 0, image.width, int(image.width / 2))
-            y = st.slider("Y", 0, image.height, int(image.height / 2))
-            size = st.slider("Size", 10, 200, 50)
-            focal_point_image = apply_focal_point(image, x, y, size)
-            st.image(focal_point_image, use_column_width=True)
-
-            # Effects (Brightness)
-            st.subheader("Effects (Brightness)")
-            factor = st.slider("Factor", 0.1, 2.0, 1.0)
-            brightness_image = apply_brightness(image, factor)
-            st.image(brightness_image, use_column_width=True)
-
-            # Overlay
-            st.subheader("Overlay")
-            overlay_image = st.file_uploader("Upload an overlay image", type=["png"])
-            if overlay_image is not None:
-                overlay = Image.open(overlay_image)
-                overlayed_image = apply_overlay(image, overlay)
-                st.image(overlayed_image, use_column_width=True)
-
-            # Frames
-            st.subheader("Frames")
-            padding = st.slider("Padding", 0, 50, 10)
-            framed_image = apply_frame(image, padding)
-            st.image(framed_image, use_column_width=True)
+            # Crop, Transform, Focal Point, Brightness, Overlay, Frames
+            # ...
 
     elif function == "AI Analysis":
         # AI Analysis and Tagging
@@ -133,7 +97,18 @@ def main():
             image_bytes = uploaded_file.read()
             output = query(image_bytes)
             # Display the generated captions
-            st.text_area("Generated Captions", value=output, height=200)
+            st.text_area("Generated Captions:", value=output, height=200)
+
+    elif function == "Image Resize":
+        # Image Resize with AI Analysis
+        uploaded_image = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
+        if uploaded_image is not None:
+            image = Image.open(uploaded_image)
+            width = st.slider("Width", 100, 2000, 800, 100)
+            height = st.slider("Height", 100, 2000, 600, 100)
+            resized_image, tags = resize_image_with_analysis(image, width, height)
+            st.image(resized_image, use_column_width=True)
+            st.write("Tags:", tags)
 
 # Run the app
 if __name__ == "__main__":
